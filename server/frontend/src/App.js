907c34e7446bbf2758633bf4e4e5ff3b6edc97f9
A.K <akcoding2020@gmail.com>
2023-04-12 19:43:32 -0400

    Final commit

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -4,9 +4,17 @@ import HomePage from './homepage.js';
 import ContactUs from './contactus.js';
 import AboutUs from './aboutus.js';
 
-/*
-Copy the Web Chat code from Watson Assitant and paste it here
-*/
+window.watsonAssistantChatOptions = {
+  integrationID: "461800c3-ee75-4fc2-a920-e89091a88e2a", // The ID of this integration.
+  region: "us-south", // The region your integration is hosted in.
+  serviceInstanceID: "6bc961f4-9d3e-4611-9671-45d2d8c61987", // The ID of your service instance.
+  onLoad: function (instance) { instance.render(); }
+};
+setTimeout(function () {
+  const t = document.createElement('script');
+  t.src = "https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
+  document.head.appendChild(t);
+});
 
 class App extends React.Component {
 
